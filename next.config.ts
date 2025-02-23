fd49d83ef23aabba89ca3bd90fee344813f6031c
robbym777 <robbymahendra777@gmail.com>
2025-02-23 22:45:27 +0700

    remove static site generator from deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Remove `output: "export"` to allow SSR
-  // output: "export",
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
